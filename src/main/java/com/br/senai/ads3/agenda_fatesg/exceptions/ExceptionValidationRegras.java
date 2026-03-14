0971c19860049db49476154057e9b265da84867e
Caio4breu <abreuocaio@gmail.com>
2026-03-13 23:56:55 -0300

    feat: adiciona estrutura inicial das classes de excecao customizadas

---
+++ src/main/java/com/br/senai/ads3/agenda_fatesg/exceptions/ExceptionValidationRegras.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.br.senai.ads3.agenda_fatesg.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Aluno
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ExceptionValidationRegras extends CoreException{
+    private String 
+}
